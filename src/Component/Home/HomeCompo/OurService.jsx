51e0c4636cbccc9d4847c8510f3f673e16f6cbda
BishwaJit07 <bishwajitr50@gmail.com>
2023-05-21 15:00:33 +0600

    service responsive

--- src/Component/Home/HomeCompo/OurService.jsx
+++ src/Component/Home/HomeCompo/OurService.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const OurService = () => {
     return (
-        <div className='bg-white flex p-4 mt-4 rounded justify-around'>
+        <div className='bg-white flex p-4 mt-4 flex-col justify-center items-center md:flex-row rounded '>
             <img src="https://i.ibb.co/27vwHMh/Untitled-1800-x-200-px-2000x-removebg-preview-1.png" alt="" className='w-40 '/>
             <img src="https://i.ibb.co/6tjWXwr/Untitled-1800-x-200-px-2000x-removebg-preview-2.png" alt="" className='w-40 '/>
             <img src="https://i.ibb.co/GHc5QJ2/Untitled-1800-x-200-px-2000x-removebg-preview-3.png" alt="" className='w-40 '/>
